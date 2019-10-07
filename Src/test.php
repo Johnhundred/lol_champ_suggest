e998800d8784a5dd4d8cd85f9fb39537ca7d4d60
Johnhundred <significantowlowl@gmail.com>
2019-10-07 12:04:40 +0200

    Trying to fix query var

--- Src/test.php
+++ Src/test.php
@@ -7,7 +7,7 @@ echo getenv('PAPERTRAIL_API_TOKEN') . PHP_EOL;
 
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Endscape', [
-    'query' => ['api_key', getenv('RIOT_API_TOKEN')]
+    'query' => 'api_key='.getenv('RIOT_API_TOKEN'),
 ]);
 echo $res->getStatusCode() . PHP_EOL;
 echo $res->getHeader('content-type')[0] . PHP_EOL;
