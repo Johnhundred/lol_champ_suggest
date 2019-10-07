2670af7f7027485a19261a3dda1bf81169b464d6
Johnhundred <significantowlowl@gmail.com>
2019-10-07 12:02:11 +0200

    Test api connection

--- Src/test.php
+++ Src/test.php
@@ -1,4 +1,14 @@
 <?php
 
+require_once __DIR__.'/../vendor/autoload.php';
+
 echo 'wat' . PHP_EOL;
 echo getenv('PAPERTRAIL_API_TOKEN') . PHP_EOL;
+
+$client = new GuzzleHttp\Client();
+$res = $client->request('GET', 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Endscape', [
+    'query' => ['api_key', getenv('RIOT_API_TOKEN')]
+]);
+echo $res->getStatusCode() . PHP_EOL;
+echo $res->getHeader('content-type')[0] . PHP_EOL;
+echo $res->getBody() . PHP_EOL;
