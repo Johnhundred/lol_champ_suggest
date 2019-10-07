57ab15437d7241117f59dd65f1ded1f229b4f1bc
Johnhundred <significantowlowl@gmail.com>
2019-10-07 16:59:04 +0200

    Testing, adding docker setup

--- Src/test.php
+++ Src/test.php
@@ -2,13 +2,20 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-echo 'wat' . PHP_EOL;
-echo getenv('PAPERTRAIL_API_TOKEN') . PHP_EOL;
-
 $client = new GuzzleHttp\Client();
 $res = $client->request('GET', 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Endscape', [
     'query' => 'api_key='.getenv('RIOT_API_TOKEN'),
 ]);
 echo $res->getStatusCode() . PHP_EOL;
 echo $res->getHeader('content-type')[0] . PHP_EOL;
-echo $res->getBody() . PHP_EOL;
+echo $res->getBody() . PHP_EOL . PHP_EOL;
+
+$json = json_decode($res->getBody());
+echo $json->profileIconId . PHP_EOL;
+echo $json->name . PHP_EOL;
+echo $json->puuid . PHP_EOL;
+echo $json->summonerLevel . PHP_EOL;
+echo $json->accountId . PHP_EOL;
+echo $json->id . PHP_EOL;
+echo $json->revisionDate . PHP_EOL;
+echo round($json->revisionDate/1000) . PHP_EOL;
